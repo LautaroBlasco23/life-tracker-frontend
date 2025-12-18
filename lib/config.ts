4073a5ac91e2d3d0165e1fccfc16ab4ab30924c4
lautaroblasco23 <lautaroblasco98@gmail.com>
2025-12-17 23:51:32 -0300

    updated getConfig function to get the next public api url

--- lib/config.ts
+++ lib/config.ts
@@ -2,15 +2,6 @@ interface Config {
   apiUrl: string;
 }
 
-export const getConfig = (): Config => {
-  if (typeof window !== 'undefined') {
-    return {
-      apiUrl:
-        (window as Record<string, any>).__ENV__?.NEXT_PUBLIC_API_URL ||
-        'http://localhost:8080/api',
-    };
-  }
-  return {
-    apiUrl: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8080/api',
-  };
-};
+export const getConfig = (): Config => ({
+  apiUrl: process.env.NEXT_PUBLIC_API_URL ?? 'http://localhost/api',
+});
