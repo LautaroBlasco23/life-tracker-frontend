79deee9ea97d92fa3b35389aea311ab6cfdec852
lautaroblasco23 <lautaroblasco98@gmail.com>
2025-12-26 10:41:47 -0300

    added timeZone attribute to user type

--- types/user.ts
+++ types/user.ts
@@ -1,9 +1,11 @@
 export interface User {
-  id: number
-  firstName: string
-  lastName: string
-  email: string
-  profilePicUrl?: string
-  createdAt: string
-  updatedAt: string
+  id: number;
+  firstName: string;
+  lastName: string;
+  email: string;
+  profilePicUrl?: string;
+  thumbnailUrl?: string;
+  timezone?: string;
+  createdAt: string;
+  updatedAt: string;
 }
